870dbd52274a632c8887677214978322ce216ed6
Lachlan Donald <lachlan@ljd.cc>
2012-12-11 17:56:10 -0800

    Add missing example object

---
+++ tests/Pheasant/Tests/Examples/AnimalWithNameDefault.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Pheasant\Tests\Examples;
+
+use \Pheasant\DomainObject;
+use \Pheasant\Types;
+use \Pheasant\Mapper\RowMapper;
+
+class AnimalWithNameDefault extends DomainObject
+{
+	public static function initialize($builder, $pheasant)
+	{
+		$pheasant
+			->register(__CLASS__, new RowMapper('animal'));
+
+		$builder
+			->properties(array(
+				'id' => new Types\Integer(11, 'primary auto_increment'),
+				'type' => new Types\String(255, 'required default=llama'),
+				'name' => new Types\String(255, 'default=blargh'),
+			));
+	}
+}
