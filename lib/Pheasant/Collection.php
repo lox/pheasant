lox/pheasant

commit e6375cd1ae4fa57ed571549846857cf724a1c734

Collection->one() no longer triggers a count query.

lib/Pheasant/Query/QueryIterator.php
@@ -116,7 +116,7 @@ class QueryIterator implements \SeekableIterator, \Countable
      */
     public function count()
     {
-        return $this->_query->count();
+        return $this->_resultSet()->count();
     }
 
     /**

commit f633cf8c24953abf0d614c6897f2ccf38ae6f4a6

Fixed performance regression from e6375cd.

lib/Pheasant/Collection.php
@@ -12,6 +12,7 @@ class Collection implements \IteratorAggregate, \Countable, \ArrayAccess
     private $_add=false;
     private $_readonly=false;
     private $_schema;
+    private $_count;
 
     /**
      * @param $class string the classname to hydrate
@@ -34,10 +35,13 @@ class Collection implements \IteratorAggregate, \Countable, \ArrayAccess
      */
     public function one()
     {
-        if($this->count() != 1)
-            throw new ConstraintException("Expected only 1 element, found ".$this->count());
+        $object = $this->offsetGet(0);
 
-        return $this->offsetGet(0);
+        // execute after the query so we save a query
+        if(($count = $this->count()) != 1)
+            throw new ConstraintException("Expected only 1 element, found $count");
+
+        return $object;
     }
 
     public function last()
@@ -93,7 +97,10 @@ class Collection implements \IteratorAggregate, \Countable, \ArrayAccess
      */
     public function count()
     {
-        return $this->_iterator->count();
+        if(!isset($this->_count))
+            $this->_count = $this->_iterator->count();
+
+        return $this->_count;
     }
 
     /**

lib/Pheasant/Query/Query.php
@@ -194,7 +194,7 @@ class Query implements \IteratorAggregate, \Countable
      */
     public function execute()
     {
-        return $this->_connection->execute($this->toSql());
+        return $this->_resultset = $this->_connection->execute($this->toSql());
     }
 
     /* (non-phpdoc)
@@ -210,7 +210,7 @@ class Query implements \IteratorAggregate, \Countable
      */
     public function __call($method, $params)
     {
-        if(!in_array($method, array('row', 'scalar', 'column', 'count')))
+        if(!in_array($method, array('row', 'scalar', 'column')))
             throw new \BadMethodCallException("$method not implemented on Query or ResultSet");
 
         return call_user_func_array(array($this->execute(), $method), $params);
@@ -254,12 +254,12 @@ class Query implements \IteratorAggregate, \Countable
 
     public function count()
     {
-        // if there is a limit or a groupBy, fall back to row count
-        if (!empty($this->_limit)) {
+        if(isset($this->_resultset)) {
+            return $this->_resultset->count();
+        } else if(isset($this->_limit)) {
             return $this->execute()->count();
         } else {
             $query = clone $this;
-
             return $query->select("count(*) count")->execute()->scalar();
         }
     }

lib/Pheasant/Query/QueryIterator.php
@@ -116,7 +116,7 @@ class QueryIterator implements \SeekableIterator, \Countable
      */
     public function count()
     {
-        return $this->_resultSet()->count();
+        return $this->_query->count();
     }
 
     /**

tests/Pheasant/Tests/CollectionTest.php
@@ -2,6 +2,8 @@
 
 namespace Pheasant\Tests;
 
+use \Pheasant\Collection;
+use \Pheasant\Query\Query;
 use \Pheasant\Tests\Examples\Animal;
 
 class CollectionTest extends \Pheasant\Tests\MysqlTestCase