b38ede4a97e4995b5e21c58c74f50e1763f837d9
Stuart Campbell <stuart@harto.org>
2013-10-08 13:53:28 +1100

    Provide method to compare identities

---
+++ tests/Pheasant/Tests/IdentityTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Pheasant\Tests;
+
+use Pheasant\Tests\Examples\Animal;
+
+class IdentityTest extends \Pheasant\Tests\MysqlTestCase
+{
+    public function testEqualIdentities()
+    {
+        $animal1 = new Animal(array('id' => 1, 'type' => 'llama'));
+        $animal2 = new Animal(array('id' => 1, 'type' => 'goat'));
+        $this->assertTrue($animal1->identity()->equals($animal2->identity()));
+    }
+
+    public function testUnequalIdentities()
+    {
+        $animal1 = new Animal(array('id' => 1, 'type' => 'llama'));
+        $animal2 = new Animal(array('id' => 2, 'type' => 'llama'));
+        $this->assertFalse($animal1->identity()->equals($animal2->identity()));
+    }
+}
