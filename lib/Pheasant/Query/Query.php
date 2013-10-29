09d76489dfa397a4fb63d898e949aad990c3b93d
Bjorn Post <bjorn.post@gmail.com>
2013-10-29 20:49:05 +0100

    Fix orderBy(): now resets the ordering.
    `orderBy()` replaces the existing ordering array.
    `andOrderBy()` adds an extra sorting key.

--- lib/Pheasant/Query/Query.php
+++ lib/Pheasant/Query/Query.php
@@ -150,7 +150,7 @@ class Query implements \IteratorAggregate, \Countable
     public function orderBy($sql, $params=array())
     {
         $binder = new Binder();
-        $this->_order[] = $binder->magicBind($sql, (array) $params);
+        $this->_order = array($binder->magicBind($sql, (array) $params));
 
         return $this;
     }
@@ -161,7 +161,10 @@ class Query implements \IteratorAggregate, \Countable
      */
     public function andOrderBy($sql, $params=array())
     {
-        return $this->orderBy($sql, $params);
+        $binder = new Binder();
+        $this->_order[] = $binder->magicBind($sql, (array) $params);
+
+        return $this;
     }
 
     /**
